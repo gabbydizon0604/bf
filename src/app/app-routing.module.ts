3a14ae2944dea5b4016c30bf8ed2e3a8f29e4807
gabbydizon <gabbydizon@email.com>
2025-12-19 09:53:25 +0900

    Fix: login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -48,6 +48,10 @@ const routes: Routes = [
       }
     ]
   },
+  {
+    path: '**',
+    redirectTo: ''
+  }
 ];
 
 @NgModule({
--- src/app/main/account/account-routing.module.ts
+++ src/app/main/account/account-routing.module.ts
@@ -39,6 +39,11 @@ const routes: Routes = [
     component: ResultadosPageComponent,
     canActivate: [GuardService]
   },
+  {
+    path: 'posiciones',
+    component: TableroPosicionesPageComponent,
+    canActivate: [GuardService]
+  }
 
 ];
 
